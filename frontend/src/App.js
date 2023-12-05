0d8615528affc5a821a0518fd03c165759de1555
Damon Davison <damon@allolex.net>
2023-12-05 11:40:38 -0500

    Check password using password_qa endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {useForm} from "react-hook-form"
+import {yupResolver} from "@hookform/resolvers/yup"
+import * as Yup from "yup"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiHost = "localhost:3005"
+// TODO: Change to https in production
+const passwordCheckUrl = `http://${apiHost}/password_qa`
+
+export default function App() {
+    const validationSchema = Yup.object().shape({
+        password: Yup.string()
+            .required("Password is required")
+            .test(
+                "password-backend-validation",
+                "Weak password",
+                async (password) => {
+                    const {data: {passed_qa}} = await axios.post(
+                        passwordCheckUrl,
+                        {password: password},
+                    )
+
+                    return passed_qa
+                }
+            ),
+        confirmPassword: Yup.string()
+            .required("Password confirmation is required")
+            .oneOf([Yup.ref("password")], "Passwords must match")
+
+    })
+
+    const formOptions = {resolver: yupResolver(validationSchema)}
+
+    // get functions to build form with useForm() hook
+    const {register, handleSubmit, reset, formState} = useForm(formOptions)
+    const {errors} = formState
+
+    function onSubmit(data) {
+        // display form data on success
+        alert("SUCCESS!! :-)\n\n" + JSON.stringify(data, null, 4))
+        return false
+    }
+
+    return (
+        <div className="card m-3">
+            <h5 className="card-header">React Hook Form - Password and Confirm Password Match Validation</h5>
+            <div className="card-body">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-row">
+                        <div className="form-group col">
+                            <label>Password</label>
+                            <input name="password" type="password" {...register("password")}
+                                className={`form-control ${errors.password ? "is-invalid" : ""}`}/>
+                            <div className="invalid-feedback">{errors.password?.message}</div>
+                        </div>
+                        <div className="form-group col">
+                            <label>Confirm Password</label>
+                            <input name="confirmPassword" type="password" {...register("confirmPassword")}
+                                className={`form-control ${errors.confirmPassword ? "is-invalid" : ""}`}/>
+                            <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary mr-1">Register</button>
+                        <button type="button" onClick={() => reset()} className="btn btn-secondary">Reset</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export {App}
