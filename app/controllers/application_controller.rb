6f082a31d2a8732baafa28b59873b91b269bdee7
Damon Davison <damon@allolex.net>
2023-12-05 11:39:22 -0500

    Allow requests from SPA to bypass token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
-  before_action :require_token
+  rescue_from ArgumentError do |err|
+    render json: { errors: [err.message] }, status: :bad_request
+  end
+
+  before_action :require_token, unless: :skip_token_check?
 
   private
 
+  def skip_token_check?
+    single_page_app_origins.include? request.headers.fetch("HTTP_ORIGIN")
+  end
+
+  def single_page_app_origins
+    [ENV.fetch("SPA_ORIGIN")]
+  end
+
   def require_token
-    return if request.xhr?
     token = Token.new(request.headers["HTTP_AUTHORIZATION"])
     head :forbidden unless token && token.valid?
   end
